bd13c124c461121c7b62f115b7caf9972e75cec9
rakeshvj <elite.rakesh@gmail.com>
2023-11-03 11:44:10 +0530

    Even number code

---
+++ even.py
@@ -0,0 +1,9 @@
+#Print the even numbers in the given N
+N = int(input(" Enter the Number N : "))
+count = 1
+while count <= N:
+    if count % 2 == 0:
+        print(count , end ="  " )
+    count += 1
+print("*"*100)
+
